5273f36e1ed9c5c363f03ad8862f6299d971bb0d
Anh Nguyen <anhnt38@gmail.com>
2016-02-26 14:00:31 +0700

    Build video comp

--- dist/js/keditor-components-1.1.1.js
+++ dist/js/keditor-components-1.1.1.js
@@ -159,6 +159,105 @@
 })(jQuery);
 
 /**
+ * KEditor Google Map Component
+ * @copyright: Kademi (http://kademi.co)
+ * @author: Kademi (http://kademi.co)
+ * @version: 1.1.1
+ * @dependencies: $, $.fn.draggable, $.fn.droppable, $.fn.sortable, Bootstrap, FontAwesome (optional)
+ */
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['googlemap'] = {
+        init: function (contentArea, container, component, options) {
+            // Do nothing
+        },
+
+        getContent: function (component, options) {
+            flog('getContent "googlemap" component', component);
+
+            var componentContent = component.children('.keditor-component-content');
+            componentContent.find('.googlemap-cover').remove();
+
+            return componentContent.html();
+        },
+
+        destroy: function (component, options) {
+            // Do nothing
+        },
+
+        settingEnabled: true,
+
+        settingTitle: 'Google Map Viewer',
+
+        initSettingForm: function (form, options) {
+            flog('initSettingForm "googlemap" component');
+
+            form.append(
+                '<form class="form-horizontal">' +
+                '   <div class="form-group">' +
+                '       <div class="col-sm-12">' +
+                '           <button type="button" class="btn btn-block btn-primary btn-googlemap-edit">Update Map</button>' +
+                '       </div>' +
+                '   </div>' +
+                '   <div class="form-group">' +
+                '       <label class="col-sm-12">Aspect Ratio</label>' +
+                '       <div class="col-sm-12">' +
+                '           <button type="button" class="btn btn-sm btn-default btn-googlemap-169">16:9</button>' +
+                '           <button type="button" class="btn btn-sm btn-default btn-googlemap-43">4:3</button>' +
+                '       </div>' +
+                '   </div>' +
+                '</form>'
+            );
+
+            var btnEdit = form.find('.btn-googlemap-edit');
+            btnEdit.on('click', function (e) {
+                e.preventDefault();
+
+                var inputData = prompt('Please enter Google Map embed code in here:');
+                var iframe = $(inputData);
+                var src = iframe.attr('src');
+                if (iframe.length > 0 && src && src.length > 0) {
+                    KEditor.settingComponent.find('.embed-responsive-item').attr('src', src);
+                } else {
+                    alert('Your Google Map embed code is invalid!');
+                }
+            });
+
+            var btn169 = form.find('.btn-googlemap-169');
+            btn169.on('click', function (e) {
+                e.preventDefault();
+
+                KEditor.settingComponent.find('.embed-responsive').removeClass('embed-responsive-4by3').addClass('embed-responsive-16by9');
+            });
+
+            var btn43 = form.find('.btn-googlemap-43');
+            btn43.on('click', function (e) {
+                e.preventDefault();
+
+                KEditor.settingComponent.find('.embed-responsive').removeClass('embed-responsive-16by9').addClass('embed-responsive-4by3');
+            });
+        },
+
+        showSettingForm: function (form, component, options) {
+            flog('showSettingForm "googlemap" component', component);
+
+            var embedItem = component.find('.embed-responsive-item');
+            var chkAutoplay = form.find('#googlemap-autoplay');
+            var src = embedItem.attr('src');
+
+            chkAutoplay.prop('checked', src.indexOf('autoplay=1') !== -1);
+        },
+
+        hideSettingForm: function (form) {
+            // Do nothing
+        }
+    };
+
+})(jQuery);
+
+/**
  * KEditor Photo Component
  * @copyright: Kademi (http://kademi.co)
  * @author: Kademi (http://kademi.co)
@@ -660,28 +759,7 @@
 
     KEditor.components['vimeo'] = {
         init: function (contentArea, container, component, options) {
-            flog('init "vimeo" component', component);
-
-            var componentContent = component.children('.keditor-component-content');
-            var wrapper = componentContent.find('.vimeo-wrapper');
-            var img = wrapper.find('img');
-
-            wrapper.append('<div class="vimeo-cover" style="display: none"></div>');
-
-            var cover = wrapper.find('.vimeo-cover');
-            wrapper.on({
-                mouseleave: function () {
-                    cover.css('display', 'none');
-                },
-                mouseenter: function () {
-                    cover.css('display', 'block');
-                }
-            });
-            cover.on('click', function (e) {
-                e.preventDefault();
-
-                cover.css('display', 'none');
-            });
+            // Do nothing
         },
 
         getContent: function (component, options) {
@@ -796,28 +874,7 @@
 
     KEditor.components['youtube'] = {
         init: function (contentArea, container, component, options) {
-            flog('init "youtube" component', component);
-
-            var componentContent = component.children('.keditor-component-content');
-            var wrapper = componentContent.find('.youtube-wrapper');
-            var img = wrapper.find('img');
-
-            wrapper.append('<div class="youtube-cover" style="display: none"></div>');
-
-            var cover = wrapper.find('.youtube-cover');
-            wrapper.on({
-                mouseleave: function () {
-                    cover.css('display', 'none');
-                },
-                mouseenter: function () {
-                    cover.css('display', 'block');
-                }
-            });
-            cover.on('click', function (e) {
-                e.preventDefault();
-
-                cover.css('display', 'none');
-            });
+            // Do nothing
         },
 
         getContent: function (component, options) {

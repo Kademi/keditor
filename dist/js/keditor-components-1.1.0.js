9aa4cff3601f0d6f3e29857d1805a09175491064
DucDHM <ducdhm@gmail.com>
2016-03-10 13:05:42 +0700

    Update

--- dist/js/keditor-components-1.1.0.js
+++ dist/js/keditor-components-1.1.0.js
@@ -489,6 +489,8 @@
     var KEditor = $.keditor;
     var flog = KEditor.log;
 
+    CKEDITOR.disableAutoInline = true;
+
     // Text component
     // ---------------------------------------------------------------------
     KEditor.components['text'] = {

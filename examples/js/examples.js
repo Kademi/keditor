d472f91eb53e3ddac0f968c37ba62bbd7e152142
Duc Doan Hoang Minh (IT - ONL) <ducdhm@vpbank.com.vn>
2018-10-23 15:49:42 +0700

    Update examples.js and examples.css

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -1,41 +1,58 @@
 (function ($) {
     $(function () {
         initModalSource();
-        initViewSourceBtn();
+        initModalContent();
+        initToolbar();
     });
-    
-    function initViewSourceBtn() {
-        var btn = $('<button type="button" class="view-source"><i class="fa fa-code"></i> View source</button>');
-        btn.appendTo(document.body);
-        btn.on('click', function () {
-	var modal = $('#modal-source');
-        var htmlCode = $('#content-area').keditor('getContent');
-//        var htmlCode = $('[data-keditor="html"]').html();
-        var htmlInclude = $('<div />').html($('[data-keditor="html-include"]').clone()).html();
-        htmlInclude = htmlInclude.replace('data-keditor="html-include"', '');
-        htmlCode += htmlInclude;
-        htmlCode = html_beautify(htmlCode, {
-            'indent_size': '4',
-            'indent_char': ' ',
-            'space_after_anon_function': true,
-            'end_with_newline': true
-        });
-        htmlCode = htmlCode.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        modal.find('#html .prettyprint').html(PR.prettyPrintOne(htmlCode,"lang-html"));
 
-        var jsCode = $('[data-keditor="script"]').html();
-        jsCode = js_beautify(jsCode, {
-            'indent_size': '4',
-            'indent_char': ' ',
-            'space_after_anon_function': true,
-            'end_with_newline': true
+    function initToolbar() {
+        var toolbar = $('<div class="toolbar"></div>');
+        var btnViewSource = $('<button type="button" class="view-source"><i class="fa fa-code"></i> View source</button>');
+        var btnViewContent = $('<button type="button" class="view-content"><i class="fa fa-file-text-o"></i> Get content</button>');
+
+        toolbar.appendTo(document.body);
+        toolbar.append(btnViewSource);
+        toolbar.append(btnViewContent);
+
+        btnViewSource.on('click', function () {
+            $('#modal-source').modal('show');
         });
-        jsCode = jsCode.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        modal.find('#js .prettyprint').html(PR.prettyPrintOne(jsCode,"lang-js"));
-        $('#modal-source').modal('show');
+
+        btnViewContent.on('click', function () {
+            var modal = $('#modal-content');
+            modal.find('.content-html').html(
+                beautifyHtml(
+                    $('#editor').keditor('getContent')
+                )
+            );
+
+            modal.modal('show');
         });
     }
-    
+
+    function initModalContent() {
+        var modal = $(
+            '<div id="modal-content" class="modal fade" tabindex="-1">' +
+            '    <div class="modal-dialog modal-lg">' +
+            '        <div class="modal-content">' +
+            '            <div class="modal-header">' +
+            '                <button type="button" class="close" data-dismiss="modal">&times;</button>' +
+            '                <h4 class="modal-title">Content</h4>' +
+            '            </div>' +
+            '            <div class="modal-body">' +
+            '                <pre class="prettyprint lang-html content-html"></pre>' +
+            '            </div>' +
+            '            <div class="modal-footer">' +
+            '                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+            '            </div>' +
+            '        </div>' +
+            '    </div>' +
+            '</div>'
+        );
+
+        modal.appendTo(document.body);
+    }
+
     function initModalSource() {
         var modal = $(
             '<div id="modal-source" class="modal fade" tabindex="-1">' +
@@ -47,15 +64,15 @@
             '            </div>' +
             '            <div class="modal-body">' +
             '                <ul class="nav nav-tabs">' +
-            '                    <li class="active"><a href="#html" data-toggle="tab"><i class="fa fa-html5"></i> HTML</a></li>' +
-            '                    <li ><a href="#js" data-toggle="tab"><i class="fa fa-code"></i> JavaScript</a></li>' +
+            '                    <li class="active"><a href="#source-html" data-toggle="tab"><i class="fa fa-html5"></i> HTML</a></li>' +
+            '                    <li ><a href="#source-js" data-toggle="tab"><i class="fa fa-code"></i> JavaScript</a></li>' +
             '                </ul>' +
             '                <div class="tab-content">' +
-            '                    <div class="tab-pane active" id="html">' +
-            '                        <pre class="prettyprint lang-html"></pre>' +
+            '                    <div class="tab-pane active" id="source-html">' +
+            '                        <pre class="prettyprint lang-html source-html"></pre>' +
             '                    </div>' +
-            '                    <div class="tab-pane" id="js">' +
-            '                        <pre class="prettyprint lang-js"></pre>' +
+            '                    <div class="tab-pane" id="source-js">' +
+            '                        <pre class="prettyprint lang-js source-js"></pre>' +
             '                    </div>' +
             '                </div>' +
             '            </div>' +
@@ -66,7 +83,39 @@
             '    </div>' +
             '</div>'
         );
+
+        var htmlCode = $('[data-keditor="html"]').html();
+        var htmlInclude = $('<div />').html($('[data-keditor="html-include"]').clone()).html();
+        htmlInclude = htmlInclude.replace('data-keditor="html-include"', '');
+        modal.find('.source-html').html(beautifyHtml(htmlCode + htmlInclude));
+
+        var jsCode = $('[data-keditor="script"]').html();
+        modal.find('.source-js').html(beautifyJs(jsCode));
+
         modal.appendTo(document.body);
     }
-    
+
+    function beautifyHtml(htmlCode) {
+        htmlCode = html_beautify(htmlCode, {
+            'indent_size': '4',
+            'indent_char': ' ',
+            'space_after_anon_function': true,
+            'end_with_newline': true
+        });
+        htmlCode = htmlCode.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+        return PR.prettyPrintOne(htmlCode, 'lang-html');
+    }
+
+    function beautifyJs(jsCode) {
+        jsCode = js_beautify(jsCode, {
+            'indent_size': '4',
+            'indent_char': ' ',
+            'space_after_anon_function': true,
+            'end_with_newline': true
+        });
+
+        return PR.prettyPrintOne(jsCode, 'lang-js');
+    }
+
 })(jQuery);

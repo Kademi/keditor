56058eee809334015d124b6227ad73d30dfa8bf0
Duc Doan <ducdhm@gmail.com>
2019-06-20 15:15:18 +0700

    https://github.com/Kademi/keditor/issues/166

--- src/keditor/topbar/initFullscreenAction.js
+++ src/keditor/topbar/initFullscreenAction.js
@@ -4,6 +4,7 @@ import 'jquery.fullscreen';
 
 export default function () {
     let self = this;
+    let options = self.options;
     let btnFullscreen = $(`<a href="javascript:void(0);" title="${options.locale.fullscreenOff}" class="${CLASS_NAMES.UI} ${CLASS_NAMES.TOPBAR_BUTTON}">${ICONS.FULLSCREEN_OFF}</a>`);
     btnFullscreen.on('click', function (e) {
         e.preventDefault();

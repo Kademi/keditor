f64c6e58e9d352254544d9b990a548d9042f698d
shivishbrahma <pur.cho.99@gmail.com>
2020-05-08 01:45:15 +0530

    Fixed exit fullscreen

--- src/keditor/topbar/initFullscreenAction.js
+++ src/keditor/topbar/initFullscreenAction.js
@@ -8,14 +8,13 @@ export default function () {
     let btnFullscreen = self.btnFullscreen = $(`<a href="javascript:void(0);" title="${options.locale.fullscreenOff}" class="${CSS_CLASS.UI} ${CSS_CLASS.TOPBAR_BUTTON}">${ICON.FULLSCREEN_OFF}</a>`);
     btnFullscreen.on('click', function (e) {
         e.preventDefault();
-    
-        setFullScreenMode.call(self, !self.isFullscreen);
+
+        setFullScreenMode.call(self, !document.fullscreenElement);
     });
     
     document.addEventListener('fullscreenchange', function () {
         let isOn = !!document.fullscreenElement;
-        
-        self.isFullScreen = isOn;
+
         btnFullscreen.html(isOn ? ICON.FULLSCREEN_ON : ICON.FULLSCREEN_OFF);
         btnFullscreen.attr('title', isOn ? options.locale.fullscreenOn : options.locale.fullscreenOff);
     });
--- src/keditor/topbar/setFullscreenMode.js
+++ src/keditor/topbar/setFullscreenMode.js
@@ -3,7 +3,7 @@ import exitFullscreen from '../utils/exitFullscreen';
 
 export default function (isOn) {
     let self = this;
-    
+
     if (isOn) {
         enterFullscreen(self.wrapper[0]);
     } else {
